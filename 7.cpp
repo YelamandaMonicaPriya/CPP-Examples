1b77ffbe41b310197d1bdd5a54530ba95135ffd6
Yelamanda Monica Priya <67221190+YelamandaMonicaPriya@users.noreply.github.com>
2020-10-01 16:01:26 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,16 @@
+//bitwise left shift and right shift operators
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=5;
+int b=7;
+cout<<"a>>1: "<<(a>>1)<<"\n";//bitwise right shift 
+cout<<"b<<3: "<<(b<<3)<<"\n";//bitwise left shift
+return 0;	
+}
+
+/*
+a>>1: 2
+b<<3: 56
+*/
